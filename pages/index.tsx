196b059dc24b6e13ff0a568698a9155f4390593f
You Song Hou <yousonghou@gmail.com>
2022-06-25 19:25:20 -0400

    feat: add a form to send Thank You NFT using NFT Port API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
 
 function index({ cbContract }) {
+  const router = useRouter();
+
+  const [bodyGuards, setbodyGuards] = useState([]);
+
   useEffect(() => {
-    fetchBodyguard();
-  }, [])
+    if(cbContract) fetchBodyguard();
+  }, [cbContract])
   
   const fetchBodyguard = async () => {
-    // const totalNFTs = await cbContract.methods._nftIds().call()
-    // console.log(totalNFTs)
-    // let temp = [];
-    // for(let i = 1; i < totalNFTs; i++){
-    //   const data = await cbContract.methods.contractNFTList(i).call()
-    //   console.log(data)
-    // }
+    const total = await cbContract.methods.bodyGuardCount().call()
+    console.log(total)
+    let temp = [];
+    for(let i = 1; i < total; i++){
+      const data = await cbContract.methods.bodyGuardList(i).call()
+      console.log(data);
+      temp.push(data);
+    }
+    setbodyGuards(temp);
   }
 
   return (
     <div className='container'>
-      
+      <h1>List of BodyGuard</h1>
+      <div className='row'>
+        {bodyGuards.map(b => (
+           <div className='col-6' key={b.id}>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{b.from}</h5>
+                <p className="card-text">{b.city}</p>
+                <button className="btn btn-primary"  onClick={() => router.push(`/sendThankyou/${b.from}`)}>
+                  Send Thank You NFT
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ pages/sendThankyou/[address].js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import { useRouter } from 'next/router';
+
+function sendThankyou() {
+  const router = useRouter();
+  const { address } = router.query;
+
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  const [cardURL, setCardURL] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [transactionUrl, setTransactionUrl] = useState('');
+
+  async function sendCard({  }) {
+    try{
+      setLoading(true);
+      const options = {
+        method: 'POST',
+        url: 'https://api.nftport.xyz/v0/mints/easy/urls',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: process.env.NEXT_PUBLIC_NFTPORT_API
+        },
+        data: {
+          chain: 'polygon',
+          name: name,
+          description: text,
+          file_url: cardURL,
+          mint_to_address: address
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        setTransactionUrl(response.data.transaction_external_url);
+        setLoading(false);
+      }).catch(function (error) {
+        console.error(error);
+        setLoading(false);
+      });
+      
+    } catch(error) {
+       console.error(error)
+       setLoading(false);
+    }  
+  }
+
+  return (
+    <div className="card card-body m-auto w-50 mt-3">
+      <h2>Send Thank You Card</h2>
+      <p>* Free Minting on Polygon</p>
+      <div className="mb-3">
+        <label for="name" className="form-label">Name</label>
+        <input className="form-control" id="name" onChange={(e) => setName(e.target.value)}/>
+      </div>
+      <div className="mb-3">
+        <label for="detail" className="form-label">Detail</label>
+        <input className="form-control" id="detail" onChange={(e) => setText(e.target.value)}/>
+      </div>
+      <div className="mb-3">
+        <label for="url" className="form-label">URL of the Card</label>
+        <input className="form-control" id="url" onChange={(e) => setCardURL(e.target.value)}/>
+      </div>
+      <div className="mb-3">
+        {!loading
+          ? <button className="btn btn-primary btn-lg mb-3" onClick={sendCard}>
+              Send
+            </button>
+          : <p>Loading...</p>
+        }
+      </div>
+      {transactionUrl &&
+        <p className="text-success" style={{ fontSize: '1.4rem'}}>
+          Success, see transaction {" "}
+          <a href={transactionUrl} target="_blank" rel="noopener noreferrer">
+              {transactionUrl}
+          </a>
+        </p>
+      }
+    </div>
+  )
+}
+
+export default sendThankyou
