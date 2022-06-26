47983bcf944d49607ce3efc1584e1b7d1733be1c
You Song Hou <yousonghou@gmail.com>
2022-06-25 23:29:40 -0400

    feat: update CryptoBodyguard contract to set available and location for bodyguard

--- config.js
+++ config.js
@@ -1,6 +1,19 @@
 export const RINKEBY_CB_Address = "0x702a3fefe8B324e7Be41911be25Dde714077f1d9"
-export const CB_Address = "0x65903CD37C41a6f9aeb99e95ceD652c358a1150c"
+export const CB_Address = "0x644D3bE977e264C02ABBbFcA59FA78e86feeE395"
 export const CB_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_city",
+				"type": "string"
+			}
+		],
+		"name": "newBodyGuard",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -30,6 +43,42 @@ export const CB_ABI = [
 		],
 		"name": "NewBodyGuard",
 		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_longitude",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_latitude",
+				"type": "string"
+			}
+		],
+		"name": "setIsAvailable",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "setIsNotAvailable",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [],
@@ -65,25 +114,27 @@ export const CB_ABI = [
 				"type": "string"
 			},
 			{
+				"internalType": "string",
+				"name": "longitude",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "latitude",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "isAvailable",
+				"type": "bool"
+			},
+			{
 				"internalType": "address",
 				"name": "from",
 				"type": "address"
 			}
 		],
 		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_city",
-				"type": "string"
-			}
-		],
-		"name": "newBodyGuard",
-		"outputs": [],
-		"stateMutability": "nonpayable",
 		"type": "function"
 	}
 ];
--- contracts/CryptoBodyguard.sol
+++ contracts/CryptoBodyguard.sol
@@ -8,6 +8,9 @@ contract CryptoBodyguard {
     struct BodyGuard {
       uint id;
       string city;
+      string longitude;
+      string latitude;
+      bool isAvailable;
       address from;
     }
 
@@ -23,7 +26,19 @@ contract CryptoBodyguard {
     function newBodyGuard(string memory _city) public {
         bodyGuardCount += 1;
 
-        bodyGuardList[bodyGuardCount] = BodyGuard(bodyGuardCount, _city, msg.sender);
+        bodyGuardList[bodyGuardCount] = BodyGuard(bodyGuardCount, _city, "", "", false, msg.sender);
         emit NewBodyGuard(bodyGuardCount, _city, msg.sender);
+    }
+
+    function setIsAvailable(uint _id, string memory _longitude, string memory _latitude) public {
+        BodyGuard storage _currentGuard = bodyGuardList[_id];
+        _currentGuard.longitude = _longitude;
+        _currentGuard.latitude = _latitude;
+        _currentGuard.isAvailable = true;
+    }
+
+    function setIsNotAvailable(uint _id) public {
+        BodyGuard storage _currentGuard = bodyGuardList[_id];
+        _currentGuard.isAvailable = false;
     }
 }
