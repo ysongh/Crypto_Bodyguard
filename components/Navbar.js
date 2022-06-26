c22e6d165b6885d3b89580e750b394d696551d9a
You Song Hou <yousonghou@gmail.com>
2022-06-26 01:31:31 -0400

    feat: add a tag for network in the Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,17 @@
-import React  from 'react';
+import React, { useState }  from 'react';
 import Link from 'next/link';
 import Web3 from 'web3';
 
-import { CB_ABI, CB_Address } from '../config';
+import {
+  CB_ABI,
+  RINKEBY_CB_Address,
+  CB_Address,
+  OP_K_CB_Address
+} from '../config';
 
 function Navbar({ ethAddress, setETHAddress, setCBContract }) {
-  
+  const [chainName, setChainName] = useState('');
+
   const openWithMetaMask = async () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
@@ -27,8 +33,28 @@ function Navbar({ ethAddress, setETHAddress, setCBContract }) {
     const accounts = await web3.eth.getAccounts();
     setETHAddress(accounts[0]);
 
-    const contract = new web3.eth.Contract(CB_ABI,CB_Address);
-    setCBContract(contract);
+    const networkId = await web3.eth.net.getId();
+    console.log(networkId);
+
+    if(networkId === 4){
+      const contract = new web3.eth.Contract(CB_ABI, RINKEBY_CB_Address);
+      setCBContract(contract);
+      setChainName("Rinkeby")
+    }
+    else if(networkId === 69){
+      const contract = new web3.eth.Contract(CB_ABI, OP_K_CB_Address);
+      setCBContract(contract);
+      setChainName("Optimistic Kovan")
+    }
+    else if(networkId === 31949730){
+      const contract = new web3.eth.Contract(CB_ABI, CB_Address);
+      setCBContract(contract);
+      setChainName("Skale")
+    }
+    else{
+      alert("No contract for this network");
+    }
+
   }
 
   return (
@@ -61,6 +87,7 @@ function Navbar({ ethAddress, setETHAddress, setCBContract }) {
               </a>
             </li>
           </ul>
+          {chainName &&  <h5><span className="badge bg-primary mt-2 me-3">{chainName}</span></h5>}
           <button className="btn btn-outline-success" onClick={openWithMetaMask}>
             {ethAddress ? ethAddress.substring(0,8) + "..." + ethAddress.substring(34,42) : "Connect to Wallet"}
           </button>
