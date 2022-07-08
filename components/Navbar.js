ac386be1bc9ab865d7a2391b88ec8e74131ee2ab
You Song Hou <yousonghou@gmail.com>
2022-07-07 21:02:00 -0400

    refactor: add 'hidden' classname to Navbar for responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -48,20 +48,20 @@ function Navbar({ ethAddress, setETHAddress, setCBContract, setUserSigner }) {
   }
 
   return (
-    <nav className="relative container mx-auto p-3">
+    <nav className="relative container mx-auto p-2">
       <div className="flex items-center justify-between">
         <div className="pt-2">
           <Link className="navbar-brand" href="/">
             <img className="h-12 cursor-pointer" src="/logo.png" alt="Logo" />
           </Link>
         </div>
-        <div className="flex space-x-5">
-          <Link href="/">Home</Link>
+        <div className="hidden md:flex space-x-5">
+          <Link href="/" className="hover:text-blue">Home</Link>
           <Link href="/signup">Signup</Link>
           <Link href="/map">Map</Link>
         </div>
         <div className="flex">
-        {chainName &&  <span className="py-2 px-4 font-semibold italic mr-2">{chainName}</span>}
+        {chainName && <span className="py-2 px-4 font-semibold italic mr-1">{chainName}</span>}
           <button className="py-2 px-4 text-white bg-blue-600 rounded-full baseline hover:bg-blue-400" onClick={openWithMetaMask}>
             {ethAddress ? ethAddress.substring(0,8) + "..." + ethAddress.substring(34,42) : "Connect to Wallet"}
           </button>
