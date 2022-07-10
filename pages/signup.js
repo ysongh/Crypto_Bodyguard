e59a9a692ceedbff126e3fb78fcba3be7a11cbef
You Song Hou <yousonghou@gmail.com>
2022-07-09 22:44:24 -0400

    feat: add name and file input field to form for BodyGuard

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 function Signup({ ethAddress, cbContract}) {
+  const [name, setName] = useState("");
+  const [imageFile, setImageFile] = useState(null);
   const [city, setCity] = useState("");
   const [loading, setLoading] = useState(false);
   const [transactionUrl, setTransactionUrl] = useState('');
   const [guardData, setGuardData] = useState({});
-
+  
   const [location, setLocation] = useState();
 
   useEffect(() => {
@@ -22,7 +24,12 @@ function Signup({ ethAddress, cbContract}) {
         setGuardData(data);
       }
     }
-   
+  }
+
+  async function handleUpload(event) {
+    const image = event.target.files[0];
+    console.log(image);
+    setImageFile(image);
   }
 
   async function createBodyGuard() {
@@ -59,7 +66,7 @@ function Signup({ ethAddress, cbContract}) {
 
   return (
     <div className="container mx-auto">
-      {guardData.from
+      {!guardData.from
         ?  <div className="bg-white p-3 rounded shadow w-5/12 mx-auto">
             <h2 className="text-2xl mt-3 mb-2">Set Available</h2>
             <div className="mb-3">
@@ -86,11 +93,31 @@ function Signup({ ethAddress, cbContract}) {
         : <div className="bg-white p-3 rounded shadow w-5/12 mx-auto">
             <h2 className="text-2xl mt-3 mb-2">Sign up to be BodyGuard</h2>
             <div className="mb-3">
+              <label htmlFor="name" className="block font-medium text-gray-700">Name</label>
+              <input className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm" id="name" onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div className="mb-3">
               <label htmlFor="city" className="block font-medium text-gray-700">City</label>
               <input
                 className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm"
                 id="city"
                 onChange={(e) => setCity(e.target.value)}/>
+            </div>
+            <label htmlFor="userPhoto" className="block font-medium text-gray-700 mb-2">Upload your photo</label>
+            <div className="flex items-center space-x-6 mb-2">
+              <div className="shrink-0">
+                <img className="h-16 w-16 object-cover rounded-full" src={imageFile && URL.createObjectURL(imageFile)} alt="Current profile photo" />
+              </div>
+              <label className="block">
+                <span className="sr-only">Choose profile photo</span>
+                <input type="file" id="userPhoto" onChange={handleUpload} className="block w-full text-sm text-slate-500
+                  file:mr-4 file:py-2 file:px-4
+                  file:rounded-full file:border-0
+                  file:text-sm file:font-semibold
+                  file:bg-violet-50 file:text-violet-700
+                  hover:file:bg-violet-100
+                "/>
+              </label>
             </div>
             <div className="mb-3">
               {!loading
