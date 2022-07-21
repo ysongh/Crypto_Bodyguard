564c4a04045944fd54651107607300474c635120
You Song Hou <yousonghou@gmail.com>
2022-07-21 00:03:21 -0400

    refactor: update UI for the Chat page

--- pages/chat/[address].js
+++ pages/chat/[address].js
@@ -2,6 +2,10 @@ import React, { useState }  from 'react';
 import { Client } from '@xmtp/xmtp-js';
 import { useRouter } from 'next/router';
 
+const BOTH_STYLES = "inline py-1 px-2  my-2 rounded";
+const LEFT_STYLES = "bg-gray-200 " + BOTH_STYLES;
+const RIGHT_STYLES = "float-right bg-blue-300 " + BOTH_STYLES;
+
 function Chat({ userSigner, ethAddress }) {
   const router = useRouter();
   const { address } = router.query;
@@ -65,21 +69,22 @@ function Chat({ userSigner, ethAddress }) {
             </div>
           </div>
       }
-      {conversationMethod && <div className="bg-white p-3 rounded shadow">
-        <h2>Messages to {toAddress}</h2>
+      {conversationMethod && <div className="bg-white p-3 rounded shadow mx-auto mt-2" style={{ maxWidth: "800px"}}>
+        <h2 className='mb-3 text-lg'>Messages to {toAddress}</h2>
         {messagesList.map(m => (
-          <p className={m.senderAddress === ethAddress ? "text-right" : ""} key={m.id}>
-            {m.content}
-          </p>
+          <div key={m.id}>
+            <p className={m.senderAddress === ethAddress ? RIGHT_STYLES : LEFT_STYLES}>
+              {m.content}
+            </p>
+          </div>
+          
         ))}
-        <div className="mb-3">
+        <div className="mb-1">
           <input className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm" id="message" onChange={(e) => setNewMessage(e.target.value)} placeholder="Message..." />
         </div>
-        <div className="mb-3">
-          <button className="py-2 px-4 mt-1 text-white bg-blue-600 rounded baseline hover:bg-blue-400" onClick={sendMessage}>
-            Send Message
-          </button>
-        </div>
+        <button className="py-2 px-4 mt-1 text-white bg-blue-600 rounded baseline hover:bg-blue-400" onClick={sendMessage}>
+          Send Message
+        </button>
       </div>}
     </div>
   )
