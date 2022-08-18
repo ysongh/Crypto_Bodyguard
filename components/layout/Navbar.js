88e6314ee89f635a0e67620e53a5283fa0e674b3
You Song Hou <yousonghou@gmail.com>
2022-08-18 00:07:31 -0400

    fix: add z-index to the menu to put the menu above the map

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -51,7 +51,7 @@ function Navbar({ ethAddress, navbarMode, chainName, setNavbarMode }) {
                       {ethAddress.substring(0,8) + "..." + ethAddress.substring(34,42)}
                     </button>
                     
-                    {showMenu && <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                    {showMenu && <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
                       <button className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1" onClick={signOut}>
                         Sign Out
                       </button>
